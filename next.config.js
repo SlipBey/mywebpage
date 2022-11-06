5efda2738f1b1e45be8a23a197866d749bbd3316
SlipBey <52493196+SlipBey@users.noreply.github.com>
2022-11-06 21:00:43 +0300

    ⚡️Next v13

--- next.config.js
+++ next.config.js
@@ -1,51 +1,63 @@
-/* eslint-disable */
-const { withPlugins } = require("next-compose-plugins");
+/* eslint-disable @typescript-eslint/no-var-requires */
+
 const withImages = require("next-images");
 
-module.exports = withPlugins([withImages], {
-	images: {
-		domains: ["i.scdn.co", "cdn.discordapp.com"],
-	},
-	reactStrictMode: true,
-	poweredByHeader: false,
-	trailingSlash: false,
-	async redirects() {
-		return [
-			{
-				source: "/github",
-				destination: "https://github.com/SlipBey",
-				permanent: true,
-			},
-			{
-				source: "/instagram",
-				destination: "https://www.instagram.com/SlipBey",
-				permanent: true,
-			},
-			{
-				source: "/youtube",
-				destination: "https://www.youtube.com/AngelCraftNW",
-				permanent: true,
-			},
-			{
-				source: "/discord",
-				destination: "https://discord.gg/vvTZtRCK3X",
-				permanent: true,
-			},
-			{
-				source: "/linkedin",
-				destination: "https://www.linkedin.com/in/slipbey/",
-				permanent: true,
-			},
-			{
-				source: "/npm",
-				destination: "https://www.npmjs.com/~mynameisslik",
-				permanent: true,
-			},
-			{
-				source: "/spotify",
-				destination: "spotify:user/g8l0pnh50bxsyb932y052ma4h",
-				permanent: true,
-			},
-		];
-	},
-});
+/**
+ * @type {import("next").NextConfig}
+ */
+
+module.exports = () => {
+	const plugins = [withImages];
+	return plugins.reduce((acc, next) => next(acc), {
+		reactStrictMode: true,
+		poweredByHeader: false,
+		trailingSlash: false,
+		swcMinify: true,
+		i18n: {
+			locales: ["en", "tr"],
+			defaultLocale: "tr",
+		},
+		images: {
+			domains: ["i.scdn.co", "cdn.discordapp.com"],
+		},
+		async redirects() {
+			return [
+				{
+					source: "/github",
+					destination: "https://github.com/SlipBey",
+					permanent: true,
+				},
+				{
+					source: "/instagram",
+					destination: "https://www.instagram.com/SlipBey",
+					permanent: true,
+				},
+				{
+					source: "/youtube",
+					destination: "https://www.youtube.com/@SlipBey",
+					permanent: true,
+				},
+				{
+					source: "/discord",
+					destination: "https://discord.gg/vvTZtRCK3X",
+					permanent: true,
+				},
+				{
+					source: "/linkedin",
+					destination: "https://www.linkedin.com/in/slipbey/",
+					permanent: true,
+				},
+				{
+					source: "/npm",
+					destination: "https://www.npmjs.com/~mynameisslik",
+					permanent: true,
+				},
+				{
+					source: "/spotify",
+					destination: "spotify:user/g8l0pnh50bxsyb932y052ma4h",
+					permanent: true,
+				},
+			];
+		},
+	});
+};
