43e7a8dc809b8e8dd71be7ba317ffc850add9a5d
SlipBey <52493196+SlipBey@users.noreply.github.com>
2023-02-07 03:18:47 +0300

    Update works.ts

--- libs/config/works.ts
+++ libs/config/works.ts
@@ -2,6 +2,7 @@ import slipyme from "@assets/works/slipyme.svg";
 import enderrise from "@assets/works/enderrise.svg";
 import code from "@assets/works/slipymecode.svg";
 import design from "@assets/works/design.svg";
+import deprem from "@assets/works/slipymedeprem.svg";
 
 export const WORKS = [
 	{
@@ -15,6 +16,12 @@ export const WORKS = [
 		text: "Slipyme Code",
 		alt: "Slipyme'nin Yazılımcılar için ayırmış olduğu bir departman.",
 		link: "https://code.slipyme.com",
+	},
+	{
+		icon: deprem,
+		text: "Slipyme Deprem",
+		alt: "Depremde ne yapılması gerekir, yardımcı olacak kaynaklar neler? Her şeyi en düzenli şekilde listelediğimiz bir yardımcı kaynak sistemi.",
+		link: "https://deprem.slipyme.com",
 	},
 	{
 		icon: design,
