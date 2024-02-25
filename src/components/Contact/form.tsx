eb8ce5af32f3967e73900214e9d1d3c1c6073ae6
SlipBey <52493196+SlipBey@users.noreply.github.com>
2024-02-25 22:09:06 +0300

    NEXT 14

--- src/components/Contact/form.tsx
+++ src/components/Contact/form.tsx
@@ -1,15 +1,11 @@
+import { useLocaleParser } from "@/libs/localeParser";
+import { formatPhoneNumber, onContactSubmit } from "@/libs/utils/contact";
 import { FormEvent, useState } from "react";
-import { toast } from "react-toastify";
-import webhook from "webhook-discord";
 
 export const ContactForm: React.FC = () => {
-  const [disabledTrue, setDisabled] = useState(false);
 
-  const [name, setName] = useState("");
-  const [surname, setSurname] = useState("");
-  const [email, setEmail] = useState("");
-  const [subject, setSubject] = useState("istek");
-  const [message, setMessage] = useState("");
+  const parser = useLocaleParser();
+
   const [phoneNumber, setPhoneNumber] = useState("");
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -17,126 +13,79 @@ export const ContactForm: React.FC = () => {
     setPhoneNumber(formattedNumber);
   };
 
-  const formatPhoneNumber = (input: string) => {
-    const cleaned = ("" + input).replace(/\D/g, "");
-    const match = cleaned.match(/^(\d{0,3})(\d{0,3})(\d{0,4})(\d{0,4})$/);
-    if (match) {
-      const formatted = `${match[1]} ${match[2]} ${match[3]}`;
-      return formatted.trim();
-    }
-    return input;
-  };
-
-  const Hook = new webhook.Webhook(
-    "https://ptb.discord.com/api/webhooks/1135216977259008061/0V_Xn8_CpI9B0Djmg8kDMlKQ-CUKsJVgxF9LnhPR4artJUviAu8D6rFoHBskHgZ5zMGK",
-  );
-
-  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (name && surname && email && subject && message) {
-      const msg = new webhook.MessageBuilder()
-        .setName(name + " " + surname)
-        .setText(
-          `Mail: ${email} - Telefon: ${phoneNumber} - Konu: ${subject} \nMesaj: ${message}`,
-        );
-      Hook.send(msg);
-
-      setDisabled(!disabledTrue);
-      toast.success("Mesajınız başarıyla iletildi!");
-    } else {
-      toast.error("Lütfen boş alan bırakmayınız!");
-    }
-  };
 
   return (
-    <form className="grid grid-cols-1 gap-2" onSubmit={onSubmit}>
-      <div className="flex flex-col lg:flex-row gap-2">
+    <form className="flex flex-col gap-2" onSubmit={onContactSubmit}>
         <div className="flex flex-col justify-left text-left items-left">
           <label>
-            İsminiz <span className="text-red-600">*</span>
+            {parser.get("name")} <span className="text-red-600">*</span>
           </label>
           <input
             type="text"
-            onChange={(e) => setName(e.target.value)}
-            className="w-64 rounded bg-gray-100 px-2 py-2 font-medium text-gray-800 outline-none dark:bg-gray-800 dark:text-gray-200"
+            name="name"
+            className="w-full rounded bg-gray-100 px-2 py-2 font-medium text-gray-800 outline-none dark:bg-gray-800 dark:text-gray-200"
             required
-            disabled={disabledTrue}
           />
         </div>
-        <div className="flex flex-col justify-left text-left items-left">
-          <label>
-            Soyisminiz <span className="text-red-600">*</span>
-          </label>
-          <input
-            type="text"
-            onChange={(e) => setSurname(e.target.value)}
-            className="w-64 rounded bg-gray-100 px-2 py-2 font-medium text-gray-800 outline-none dark:bg-gray-800 dark:text-gray-200"
-            required
-            disabled={disabledTrue}
-          />
-        </div>
-      </div>
       <div className="flex flex-col lg:flex-row gap-2">
         <div className="flex flex-col justify-left text-left items-left">
           <label>
-            Mail Adresiniz <span className="text-red-600">*</span>
+          {parser.get("mail")} <span className="text-red-600">*</span>
           </label>
           <input
-            onChange={(e) => setEmail(e.target.value)}
             type="email"
-            className="w-64 rounded bg-gray-100 px-2 py-2 font-medium text-gray-800 outline-none dark:bg-gray-800 dark:text-gray-200"
+            name="email"
+            className="w-full rounded bg-gray-100 px-2 py-2 font-medium text-gray-800 outline-none dark:bg-gray-800 dark:text-gray-200"
             required
-            disabled={disabledTrue}
           />
         </div>
         <div className="flex flex-col justify-left text-left items-left">
-          <label>Telefon Numaranız</label>
+          <label>{parser.get("phone")}</label>
           <input
             type="tel"
-            disabled={disabledTrue}
+            name="phone"
+            className="w-full rounded bg-gray-100 px-2 py-2 font-medium text-gray-800 outline-none dark:bg-gray-800 dark:text-gray-200"
+            placeholder={parser.get("phoneText")}
+            pattern="\d{0,3} \d{0,3} \d{0,4}"
             maxLength={12}
             minLength={12}
-            placeholder="Başına 0 koymayın."
-            className="w-64 rounded bg-gray-100 px-2 py-2 font-medium text-gray-800 outline-none dark:bg-gray-800 dark:text-gray-200"
-            pattern="\d{0,3} \d{0,3} \d{0,4} \d{0,4}"
-            value={phoneNumber}
             onChange={handleChange}
+            value={phoneNumber}
           />
         </div>
       </div>
       <div className="flex flex-col justify-left text-left items-left">
         <label>
-          Konu <span className="text-red-600">*</span>
+        {parser.get("subject")} <span className="text-red-600">*</span>
         </label>
         <select
-          disabled={disabledTrue}
-          onChange={(e) => setSubject(e.target.value)}
           defaultValue="istek"
+          name="subject"
           className="w-full rounded bg-gray-100 px-2 py-2 font-medium text-gray-800 outline-none dark:bg-gray-800 dark:text-gray-200"
           required
         >
-          <option value="istek">İstek/Öneri</option>
-          <option value="teklif">İş Teklifi</option>
-          <option value="bildirim">Geri Bildirim</option>
+          <option value="istek">{parser.get("optionIstek")}</option>
+          <option value="teklif">{parser.get("optionTeklif")}</option>
+          <option value="bildirim">{parser.get("optionBildirim")}</option>
         </select>
       </div>
       <div className="flex flex-col justify-left text-left items-left">
         <label>
-          Mesajınız <span className="text-red-600">*</span>
+        {parser.get("message")} <span className="text-red-600">*</span>
         </label>
         <textarea
-          onChange={(e) => setMessage(e.target.value)}
           className="w-full rounded bg-gray-100 px-2 py-2 font-medium text-gray-800 outline-none dark:bg-gray-800 dark:text-gray-200"
+          rows={3}
+          name="message"
           required
         />
       </div>
       <div>
         <button
-          aria-label="submit"
+          type="submit"
           className="mt-2 bg-blue-600 hover:bg-blue-700 w-full p-3 rounded-lg font-semibold text-lg duration-200 text-white"
-          disabled={disabledTrue}
         >
-          Gönder
+          {parser.get("submit")}
         </button>
       </div>
     </form>
---
+++ src/models/contact.ts
@@ -0,0 +1,7 @@
+export interface IContactProps {
+    name: string;
+    email: string;
+    phone: number;
+    subject: string;
+    message: string;
+  }
